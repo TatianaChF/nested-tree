b74b279ebae7328d1a09b77c5c74461a33b7739d
tikaya <tatiana.chefox@gmail.com>
2024-02-12 15:58:45 +0300

    adding + and - signs

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ function listree() {
     const subMenuHeading = document.getElementsByClassName("listree-submenu-heading");
     Array.from(subMenuHeading).forEach(
         function(subMenuHeading) {
+            subMenuHeading.classList.ass("collapsed");
             subMenuHeading.nextElementSibling.style.display = "none";
             subMenuHeading.addEventListener('click', function(event) {
                 event.preventDefault();
                 const subMenuList = event.target.nextElementSibling;
                 if(subMenuList.style.display == "none") {
+                    subMenuHeading.classList.remove("collapsed");
+                    subMenuHeading.classList.add("expanded");
                     subMenuList.style.display = "block";
                 }
                 else {
+                    subMenuHeading.classList.remove("expanded");
+                    subMenuHeading.classList.add("collapsed");
                     subMenuList.style.display = "none";
                 }
                 event.stopPropagation();
